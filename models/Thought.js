0130ffc1d42a0444dc59d2d6c9a4b79d320513a8
TonnTribe <127579030+TonnTribe@users.noreply.github.com>
2023-09-03 00:54:35 -0700

    models established

---
+++ models/Thought.js
@@ -0,0 +1,72 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        // set custom id to avoid confusion with parent thought _id
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: 'Reaction is required.',
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: 'Username is required.'
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            // use a getter method to format the timestamp on query
+            get: (createdAtVal) => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: 'Thought is required.',
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            // use a getter method to format the timestamp on query
+            get: (createdAtVal) => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: 'Username is required.'
+        },
+        // use ReactionSchema to validate data for a reaction
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+// get total count of reactions on retrieval
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+// create the Thought model using the ThoughtSchema
+const Thought = model('Thought', thoughtSchema);
+
+// export the Thought model
+module.exports = Thought;
